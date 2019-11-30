cc04d304f60b9830d3a0fc5de4cf1f035a6ba4da
ItsCopex <56700022+ItsCopex@users.noreply.github.com>
2019-11-30 22:41:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
+
 import discord
 from discord.ext import commands
-from discord.utils import getimport os
+from discord.utils import get
+import os
 
 client = commands.Bot(command_prefix="/")
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Streaming(name="ItsCopex#9227", url="twitch.com/itscopex"))
+    await client.change_presence(activity=discord.Streaming(name="Discord.py", url="https://twitch.tv/itscopex"))
 
 @client.command()
 async def attack(ctx, *, content):
     await ctx.send(content)
+
 
 client.run(os.environ["DISCORD_TOKEN"])
