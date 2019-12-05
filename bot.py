e4960971a5a251caeb3af2bce1a16aaf86377a64
ItsCopex <56700022+ItsCopex@users.noreply.github.com>
2019-12-05 21:31:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ async def on_ready():
     await client.change_presence(activity=discord.Streaming(name="ItsCopex#9227", url="https://twitch.tv/itscopex"))
 
 @client.command()
+@commands.has_permissions(manage_emojis = True)
 async def attack(ctx, *, content):
     await ctx.send(content)
 
