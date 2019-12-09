03b76ea82cd02af72a25ee9b49bde3c99b5573fb
ItsCopex <56700022+ItsCopex@users.noreply.github.com>
2019-12-09 17:07:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ client.remove_command("help")
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Streaming(name="ItsCopex#9227", url="https://twitch.tv/itscopex"))
+    await client.change_presence(activity=discord.Streaming(name="ItsCopex#9227 add me", url="https://twitch.tv/itscopex"))
 
 @client.command()
 async def attack(ctx, *, content):
@@ -22,9 +22,9 @@ async def help():
         colour = discord.Colour.dark_green()
     )
 
-    embed.set_footer(text="Flooding !attack flood <host> <port> <version> <time>")
-    embed.set_footer(text="Instantcrashing !attack instantcrash <host> <port> <version> <time>")
-    embed.set_footer(text="Nullpingcrash !attack nullping <host> <port> <version> <time>")
+    embed.add_field(name="Flooding !attack flood <host> <port> <version> <time>")
+    embed.add_field(name="Instantcrashing !attack instantcrash <host> <port> <version> <time>")
+    embed.add_field(name="Nullpingcrash !attack nullping <host> <port> <version> <time>")
 
 
 client.run(os.environ["DISCORD_TOKEN"])
