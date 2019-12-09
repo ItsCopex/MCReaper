a9402e6d3960eead46f6173afa78d1ddd445c6e5
ItsCopex <56700022+ItsCopex@users.noreply.github.com>
2019-12-09 17:19:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,8 +23,11 @@ async def help(ctx):
         desccription = "ewflk",
         colour = discord.Colour.dark_green()
     )
-
-    embed.add_field(name="Flooding", value="!attack flood <host> <port> <version> <time>", inline=False)
+    embed.set_author(name="ItsCopex#9227),
+    embed.set_thumbnail(url="https://cdn.discordapp.com/icons/647940481698758666/64cc91c85ea0464ab87489a7e2ccd593.webp?size=128"),
+    embed.set_image(url="https://cdn.discordapp.com/icons/647940481698758666/64cc91c85ea0464ab87489a7e2ccd593.webp?size=128"),
+    icon_url="https://cdn.discordapp.com/icons/647940481698758666/64cc91c85ea0464ab87489a7e2ccd593.webp?size=128"
+    embed.add_field(name="Flooding", value="!attack flood <host> <port> <version> <time>", inline=True)
     embed.add_field(name="Instantcrashing" value="!attack instantcrash <host> <port> <version> <time>", inline=True)
     embed.add_field(name="Nullpingcrash" value="!attack nullping <host> <port> <version> <time>", inline=True)
 
